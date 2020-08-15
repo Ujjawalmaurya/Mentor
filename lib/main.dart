fcbb6870bc4cf7e9b912c75ef5ddfaf31ee0c941
mfaizk <faizk123485@gmail.com>
2020-08-15 20:05:58 +0530

    Revert "1.new Login detection logic implemented."
    This reverts commit 2a755c79e33173058eabc9e82d56e4fd1a9d25ec.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mentor_digishala/constants.dart';
 import 'package:mentor_digishala/homePage.dart';
 import 'package:mentor_digishala/loginPage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: FirebaseAuth.instance.currentUser() != null
-          ? HomePage.id
-          : LoginScreen.id,
+      initialRoute: LoginScreen.id,
       routes: {
         LoginScreen.id: (context) => LoginScreen(),
         HomePage.id: (context) => HomePage()
