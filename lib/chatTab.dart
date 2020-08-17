81db48539eaaa757dcc7d0bb6774e7abdd6a1381
mfaizk <faizk123485@gmail.com>
2020-08-17 15:23:18 +0530

    1.experimnetal signout button

--- lib/chatTab.dart
+++ lib/chatTab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mentor_digishala/loginPage.dart';
 import 'constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class chatTab extends StatefulWidget {
   @override
@@ -10,6 +11,14 @@ class chatTab extends StatefulWidget {
 class _chatTabState extends State<chatTab> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: RaisedButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+          Navigator.pushReplacementNamed(context, LoginScreen.id);
+        },
+        child: Text('logout'),
+      ),
+    );
   }
 }
