4027ee860102f329b207d2730b5f2a80e801c0bc
suneater <suneater@pop-os.localdomain>
2020-09-12 10:31:35 +0530

    Now using Provider For Signout and Sign in Event

--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,30 +1,20 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mentor_digishala/authProvider.dart';
 import 'package:mentor_digishala/homePage.dart';
 import 'package:mentor_digishala/loginPage.dart';
+import 'package:provider/provider.dart';
 
 //code used from https://medium.com/coding-with-flutter/super-simple-authentication-flow-with-flutter-firebase-737bba04924c
 class LandingPage extends StatelessWidget {
   static const String id = 'LandingPage';
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<FirebaseUser>(
-      stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          FirebaseUser user = snapshot.data;
-          if (user == null) {
-            return LoginScreen();
-          }
-          return HomePage();
-        } else {
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
-    );
+    AuthProvider authProvider = Provider.of(context);
+    if (authProvider.isAuthenticated) {
+      return HomePage();
+    } else {
+      return LoginScreen();
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentor_digishala/authProvider.dart';
 import 'package:mentor_digishala/homePage.dart';
 import 'package:mentor_digishala/landingPage.dart';
 import 'package:mentor_digishala/loginPage.dart';
@@ -7,6 +8,7 @@ import 'package:mentor_digishala/signUpPage.dart';
 import 'package:mentor_digishala/tabs/classChangeTab.dart';
 import 'package:mentor_digishala/tabs/messeging/textingTabs.dart';
 import 'package:mentor_digishala/terms.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,21 +17,30 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: LandingPage.id,
-      routes: {
-        LoginScreen.id: (context) => LoginScreen(),
-        HomePage.id: (context) => HomePage(),
-        LandingPage.id: (context) => LandingPage(),
-        "signup": (context) => SignUpPage(),
-        TextingTabs.id: (context) => TextingTabs(),
-        ClassChangeTab.id: (context) => ClassChangeTab(),
-        Policy.id: (context) => Policy(),
-        TermsnC.id: (context) => TermsnC(),
-      },
-      title: "Mentor -DigiShala",
-      theme: ThemeData.light(),
-      // home: LoginScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) {
+            return AuthProvider();
+          },
+        )
+      ],
+      child: MaterialApp(
+        initialRoute: LandingPage.id,
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          HomePage.id: (context) => HomePage(),
+          LandingPage.id: (context) => LandingPage(),
+          "signup": (context) => SignUpPage(),
+          TextingTabs.id: (context) => TextingTabs(),
+          ClassChangeTab.id: (context) => ClassChangeTab(),
+          Policy.id: (context) => Policy(),
+          TermsnC.id: (context) => TermsnC(),
+        },
+        title: "Mentor -DigiShala",
+        theme: ThemeData.light(),
+        // home: LoginScreen(),
+      ),
     );
   }
 }
